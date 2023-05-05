7148ade2b72e63a35ed74cd6805d68ac1b7911d2
Staroz <marcourquidi17@gmail.com>
2023-05-05 00:05:34 -0400

    last test

--- cypress.config.js
+++ cypress.config.js
@@ -1,27 +1,27 @@
 const { defineConfig } = require("cypress");
 
-
 module.exports = defineConfig({
-  defaultCommandTimeout: 7000,
-  viewportWidth: 1200,
-  viewportHeight: 660,
-  experimentalModifyObstructiveThirdPartyCode: true,
-    env: {
-      key: "ab88591249aebbd860f4ca1576a211f0",
-      token: "ATTAd0cc188f185548fdc1f1cc944d6966a6d78854ab3aa4d1909b61f8a37d72e296B43387C9",
-      boardName: "Test 01",
-      newBoardName: "Test 02",
-    }
+	defaultCommandTimeout: 7000,
+	viewportWidth: 1200,
+	viewportHeight: 660,
+	experimentalModifyObstructiveThirdPartyCode: true,
+	env: {
+		key: "ab88591249aebbd860f4ca1576a211f0",
+		token:
+			"ATTAd0cc188f185548fdc1f1cc944d6966a6d78854ab3aa4d1909b61f8a37d72e296B43387C9",
+		boardName: "Test 01",
+		newBoardName: "Test 02",
+	},
 
-    e2e: {
-    experimentalSessionAndOrigin: true,
-    baseUrl:"https://api.trello.com/1/boards/",
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
-    excludeSpecPattern:[
-      "**/1-getting-started/*.js",
-      "**/2-advanced-examples/*.js"
-    ],
-  },
+	e2e: {
+		experimentalSessionAndOrigin: true,
+		baseUrl: "https://api.trello.com/1/boards/",
+		setupNodeEvents(on, config) {
+			// implement node event listeners here
+		},
+		excludeSpecPattern: [
+			"**/1-getting-started/*.js",
+			"**/2-advanced-examples/*.js",
+		],
+	},
 });
--- cypress/e2e/testTrelloApi.cy.js
+++ cypress/e2e/testTrelloApi.cy.js
@@ -1,39 +1,39 @@
 /// <reference types= "cypress" />
 
-const token = Cypress.env('token');
-const key = Cypress.env('');
-const boardName = "Test 01";
-const newBoardName = "Test 02";
+//const token = Cypress.env('token');
+//const key = Cypress.env('key');
+//const boardName = Cypress.env('boardName');
+//const newBoardName = Cypress.env('newBoardName');
 
 describe("Boards manipulation whit API", function() {
 	before('Create a new Board in Trello', function() {
-        //cy.fixture('credentials.json').then(function(value) {
-        //    const token = value.token;
-        //    const key = value.key;
-		//	const boardName = value.boardName;
+        cy.fixture('credentials.json').then(function(value) {
+            const token = value.token;
+            const key = value.key;
+			const boardName = value.boardName;
             
             cy.boardCreate(key, token, boardName);
-        //});
+        });
     });
     
     it('Update board name', function() {
         
-        //cy.fixture('credentials.json').then(function(value) {
-        //    const token = value.token;
-        //    const key = value.key;
-        //    const newBoardName = value.newBoardName;
+        cy.fixture('credentials.json').then(function(value) {
+            const token = value.token;
+            const key = value.key;
+            const newBoardName = value.newBoardName;
             
             cy.boardUpdate(key, token, newBoardName);
-        //});
+        });
     });
     
     after('Delete the Board created',()=>{
-        //cy.fixture('credentials.json').then(function(value) {
-        //    const token = value.token;
-        //    const key = value.key;
+        cy.fixture('credentials.json').then(function(value) {
+            const token = value.token;
+            const key = value.key;
             
             cy.boardDelete(key, token);
-        //});
+        });
     });
 });
 
