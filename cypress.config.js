9b1934877498a8708feb6cb1318ac021ced28783
Staroz <marcourquidi17@gmail.com>
2023-05-04 23:29:33 -0400

    otro iontento

--- cypress.config.js
+++ cypress.config.js
@@ -6,6 +6,13 @@ module.exports = defineConfig({
   viewportWidth: 1200,
   viewportHeight: 660,
   experimentalModifyObstructiveThirdPartyCode: true,
+    env: {
+      key: "ab88591249aebbd860f4ca1576a211f0",
+      token: "ATTAd0cc188f185548fdc1f1cc944d6966a6d78854ab3aa4d1909b61f8a37d72e296B43387C9",
+      boardName: "Test 01",
+      newBoardName: "Test 02",
+    }
+
     e2e: {
     experimentalSessionAndOrigin: true,
     baseUrl:"https://api.trello.com/1/boards/",
--- cypress/e2e/testTrelloApi.cy.js
+++ cypress/e2e/testTrelloApi.cy.js
@@ -1,36 +1,39 @@
 /// <reference types= "cypress" />
 
-const credentials = Cypress.env('CREDENTIALS_SECRET');
+const token = Cypress.env('token');
+const key = Cypress.env('');
+const boardName = "Test 01";
+const newBoardName = "Test 02";
 
 describe("Boards manipulation whit API", function() {
 	before('Create a new Board in Trello', function() {
-        cy.fixture('credentials.json').then(function(value) {
-            const token = value.token;
-            const key = value.key;
-			const boardName = value.boardName;
+        //cy.fixture('credentials.json').then(function(value) {
+        //    const token = value.token;
+        //    const key = value.key;
+		//	const boardName = value.boardName;
             
             cy.boardCreate(key, token, boardName);
-        });
+        //});
     });
     
     it('Update board name', function() {
         
-        cy.fixture('credentials.json').then(function(value) {
-            const token = value.token;
-            const key = value.key;
-            const newBoardName = value.newBoardName;
+        //cy.fixture('credentials.json').then(function(value) {
+        //    const token = value.token;
+        //    const key = value.key;
+        //    const newBoardName = value.newBoardName;
             
             cy.boardUpdate(key, token, newBoardName);
-        });
+        //});
     });
     
     after('Delete the Board created',()=>{
-        cy.fixture('credentials.json').then(function(value) {
-            const token = value.token;
-            const key = value.key;
+        //cy.fixture('credentials.json').then(function(value) {
+        //    const token = value.token;
+        //    const key = value.key;
             
             cy.boardDelete(key, token);
-        });
+        //});
     });
 });
 
